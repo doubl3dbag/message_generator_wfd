2b74143a3331c24cdc738b068aaf30ed42beefea
doubl3dbag <69617207+doubl3dbag@users.noreply.github.com>
2021-09-20 23:13:39 -0500

    changed file name to script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //create a random meal idea to inspire the user
 
 //generate a random number
-
+const randomizeNum = () => {
+    Math.floor(Math.random() * 10);
+}
+console.log(randomizeNum(2));
 //messages the program will output
 // what meal did the user have the day prior?
 //**message 1 - You had ______ for dinner yesterday? */
