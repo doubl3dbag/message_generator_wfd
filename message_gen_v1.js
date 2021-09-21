0ce2f4666a7bbd8fbd93a32bdc3e1ee4df1de942
doubl3dbag <69617207+doubl3dbag@users.noreply.github.com>
2021-09-20 22:45:04 -0500

    comment message ideas

--- message_gen_v1.js
+++ message_gen_v1.js
@@ -1,1 +1,13 @@
 //create a random meal idea to inspire the user
+
+//generate a random number
+
+//messages the program will output
+// what meal did the user have the day prior?
+//**message 1 - You had ______ for dinner yesterday? */
+
+//what what ethnicity is the dinner today?
+//**message 2 - Your dinner will be ______ ethnicity. */
+
+//the name of the meal
+//**message 3 - ______ is the name of today's dinner! */
